ad859906c921f57652f3387b7136c0a42570e941
mpham-12 <martinp0512@gmail.com>
2022-09-19 13:57:08 -0400

    add and style login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,16 @@ import classes from './Login.module.css'
 
 const Login = () => {
   return (
-    <section>
-<h1>HIIIIIII</h1>
+    <section className={classes.login}>
+      <h2 className={classes.title}>Log in</h2>
+      <form className={classes.form}>
+        <label htmlFor="email" className={classes.label}>E-mail</label>
+        <input className={classes.input} type="email" />
+        <label htmlFor="password" className={classes.label}>Password</label>
+        <input className={classes.input} type="password" />
+      </form>
+      <p className={classes.signup}>Don't have an account? </p>
+      <a href="/signup"><p className={classes.signup} id={classes.getStarted}>Let's get started</p></a>
     </section>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,14 @@ import classes from './Nav.module.css'
 const Nav = () => {
   return (
     <nav>
-      <div className={classes.navLeft}>
-        <a href="" className={classes.navAnchor}><h1 className={classes.navName}>Better Health</h1></a>
+      <div className={classes.navLeft}></div>
+      <div className={classes.navMiddle}>
+        <a href="/" className={classes.navAnchor}><h1 className={classes.navName}>Better Health</h1></a>
       </div>
       <div className={classes.navRight}>
         <ul className={classes.navLinks}>
           <a className={classes.navAnchor} href=""><li className={classes.navLink}>Chat</li></a>
-          <a className={classes.navAnchor} href=""><li className={classes.navLink}>Login</li></a>
+          <a className={classes.navAnchor} href="/login"><li className={classes.navLink}>Login</li></a>
           <a className={classes.navAnchor} href=""><li className={classes.navLink}>Logout</li></a>
         </ul>
       </div>
